1a56ae295ab5cd61d8defec6919f964e168985a6
Richard P Sharp Jr. <richpsharp@gmail.com>
2025-12-22 14:50:33 -0800

    re #4 adding in a mean calculation

--- runner.py
+++ runner.py
@@ -605,6 +605,8 @@ def fast_zonal_statistics(
                 (feature_id_raster_path, 1), offset_only=True
             )
         )
+        logger.info(feature_id_raster_offsets)
+        sys.exit()
         logger.info(
             "iterblocks prepared | blocks=%d",
             len(feature_id_raster_offsets),
@@ -637,7 +639,6 @@ def fast_zonal_statistics(
 
         logger.info("gathering stats from raster blocks")
         block_log_time = time.time()
-        group_digest = None
         group_sketch = None
         if percentile_list:
             group_sketch = defaultdict(lambda: kll_floats_sketch(k=200))
@@ -1146,6 +1147,13 @@ def fast_zonal_statistics(
                         group_stats["max"], feature_stats["max"]
                     )
 >>>>>>> 9161e18 (re #4 adding a test harness for development)
+
+        for group_value, group_stats in grouped_stats.items():
+            valid_count = group_stats["count"] - group_stats["nodata_count"]
+            group_stats["valid_count"] = valid_count
+            group_stats["mean"] = (
+                (group_stats["sum"] / valid_count) if valid_count > 0 else None
+            )
 
         if group_sketch is not None:
             for group_value, sk in group_sketch.items():
